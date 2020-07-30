606858ff4d874ac629cbaa1b679dbb8d82393349
abhijit2251 <34393392+abhijit2251@users.noreply.github.com>
2020-07-30 13:51:59 +0530

    attachVideo update and dataURItoBlob added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from "@angular/core";
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { interval } from 'rxjs';
 import { AuthenticationService } from './authentication.service';
+
 
 @Component({
   selector: 'app-root',
@@ -14,15 +15,17 @@ export class AppComponent implements OnInit {
   @ViewChild('canvas', { static: true }) canvas: ElementRef;
   videoWidth = 0;
   videoHeight = 0;
-  captureImgArr: any = [];
+  captureImgArr = [];
 
   constructor(public fb: FormBuilder, private renderer: Renderer2, public authenticationService: AuthenticationService, private el: ElementRef) { }
 
   ngOnInit() {
     this.loginForm = this.fb.group({
-      username: [null],
-      password: [null]
+      username: [null, Validators.required],
+      password: [null, Validators.required]
     });
+
+    // this.startCamera();
   }
 
   submit(form) {
@@ -30,7 +33,6 @@ export class AppComponent implements OnInit {
     this.authenticationService.login(form.username, form.password).subscribe(res => {
       console.log("res", res)
     });
-
     this.startCamera();
   }
 
@@ -50,8 +52,16 @@ export class AppComponent implements OnInit {
     }
   }
 
+  // attachVideo(stream) {
+  //   this.renderer.setProperty(this.videoElement.nativeElement, 'srcObject', stream);
+  // }
+
   attachVideo(stream) {
     this.renderer.setProperty(this.videoElement.nativeElement, 'srcObject', stream);
+    this.renderer.listen(this.videoElement.nativeElement, 'play', (event) => {
+      this.videoHeight = this.videoElement.nativeElement.videoHeight;
+      this.videoWidth = this.videoElement.nativeElement.videoWidth;
+    });
   }
 
   handleError(error) {
@@ -62,42 +72,31 @@ export class AppComponent implements OnInit {
     this.renderer.setProperty(this.canvas.nativeElement, 'width', this.videoWidth);
     this.renderer.setProperty(this.canvas.nativeElement, 'height', this.videoHeight);
     this.canvas.nativeElement.getContext('2d').drawImage(this.videoElement.nativeElement, 0, 0);
-    this.captureImgArr.push(this.canvas.nativeElement.toDataURL("image/png"))
-    console.log("captureImgArr", this.captureImgArr)
-    interval(30000).subscribe(x => {
-      // something
-      this.capture();
-    });
-
+    this.captureImgArr.push(this.canvas.nativeElement.toDataURL("image/png"));
+    let dataURL: string = this.canvas.nativeElement.toDataURL("image/png");
+    const base64 = dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+    const imageName = 'name.png';
+    const imageBlob = this.dataURItoBlob(base64);
+    const imageFile = new File([imageBlob], imageName, { type: 'image/png' });
     let formData = new FormData();
-    for (let key in this.captureImgArr) {
-      let files: Array<File> = this.captureImgArr[key]
-      console.log("files", files[0]);
-      formData.append('ayn', files[0])
-    }
+    formData.append('ayn', imageFile)
     this.authenticationService.uploadImage(formData).subscribe(res => {
       console.log("image submitted successfully", res)
-    })
-
+    });
+    interval(30000).subscribe(x => {
+      this.capture();
+    });
   }
 
-  // THIS IS ONLY FOR TESTING PURPOSE
-  upload() {
-    let inputEl: HTMLInputElement = this.el.nativeElement.querySelector('#photo');
-    console.log("inputEl", inputEl)
-    let fileCount: number = inputEl.files.length;
-    console.log("fileCount", fileCount)
-    // return
-    let formData = new FormData();
-    if (fileCount > 0) { // a file was selected
-      for (let i = 0; i < fileCount; i++) {
-        formData.append('ayn', inputEl.files.item(i));
-      }
-      this.authenticationService.uploadImage(formData).subscribe(res => {
-        console.log("image submitted successfully", res)
-      });
+  dataURItoBlob(dataURI) {
+    const byteString = window.atob(dataURI);
+    const arrayBuffer = new ArrayBuffer(byteString.length);
+    const int8Array = new Uint8Array(arrayBuffer);
+    for (let i = 0; i < byteString.length; i++) {
+      int8Array[i] = byteString.charCodeAt(i);
     }
+    const blob = new Blob([int8Array], { type: 'image/png' });
+    return blob;
   }
-
 
 }
